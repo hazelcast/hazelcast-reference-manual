19f66786de609081e89f70d571b83f03366a89c8
Serdar Ozmen <serdar@hazelcast.com>
2016-03-07 16:25:50 +0200

    Added the required scripts and edited the layout for navigation improvements.

---
+++ themes/single_html/assets/js/toc-attributes.js
@@ -0,0 +1,13 @@
+	    	$(function() {
+                  var toc = $("#toc").tocify().data("toc-tocify");
+                  toc.setOptions({ showEffect: "show", 
+						showEffectSpeed: "fast",
+						hideEffect: "none",
+						hideEffectSpeed: "fast",
+						scrollTo: 5, 
+						smoothScroll: false, 
+						highlightOnScroll: true
+						// extendPage: true,
+						// extendPageOffset: 100,
+						});
+              });
