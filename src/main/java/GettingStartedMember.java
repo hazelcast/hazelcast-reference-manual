218775bb902c1348ff7a95b947516770b263b441
Serdar Ozmen <serdar@hazelcast.com>
2018-05-14 11:34:16 +0300

    Created "main/java" directory and completed code compilation task for Getting Started chapter.

---
+++ src/main/java/GettingStartedMember.java
@@ -0,0 +1,38 @@
+import com.hazelcast.config.Config;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+import java.util.Map;
+import java.util.Queue;
+
+public class GettingStartedMember {
+    
+   public static void main(String[] args) {
+      //tag::startmember[]
+      Config cfg = new Config();
+      HazelcastInstance instance = Hazelcast.newHazelcastInstance(cfg);
+      Map<Integer, String> mapCustomers = instance.getMap("customers");
+      mapCustomers.put(1, "Joe");
+      mapCustomers.put(2, "Ali");
+      mapCustomers.put(3, "Avi");
+ 
+      System.out.println("Customer with key 1: "+ mapCustomers.get(1));
+      System.out.println("Map Size:" + mapCustomers.size());
+ 
+      Queue<String> queueCustomers = instance.getQueue("customers");
+      queueCustomers.offer("Tom");
+      queueCustomers.offer("Mary");
+      queueCustomers.offer("Jane");
+      System.out.println("First customer: " + queueCustomers.poll());
+      System.out.println("Second customer: "+ queueCustomers.peek());
+      System.out.println("Queue size: " + queueCustomers.size());
+      //end::startmember[]
+   }
+}
+
+
+
+    
+
+
+
