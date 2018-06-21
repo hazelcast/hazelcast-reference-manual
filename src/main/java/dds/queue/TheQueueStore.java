e246af8405703a12e0998275036b6bec4edbedf1
Serdar Ozmen <serdar@hazelcast.com>
2018-06-21 16:45:08 +0300

    Code Compilation Task: Chapter 7 (cont'd)

---
+++ src/main/java/dds/queue/TheQueueStore.java
@@ -0,0 +1,48 @@
+import com.hazelcast.core.QueueStore;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+//tag::qs[]
+public class TheQueueStore implements QueueStore<Item> {
+
+    @Override
+    public void delete(Long key) {
+        System.out.println("delete");
+    }
+
+    @Override
+    public void store(Long key, Item value) {
+        System.out.println("store");
+    }
+
+    @Override
+    public void storeAll(Map<Long, Item> map) {
+        System.out.println("store all");
+    }
+
+    @Override
+    public void deleteAll(Collection<Long> keys) {
+        System.out.println("deleteAll");
+    }
+
+    @Override
+    public Item load(Long key) {
+        System.out.println("load");
+        return null;
+    }
+
+    @Override
+    public Map<Long, Item> loadAll(Collection<Long> keys) {
+        System.out.println("loadALl");
+        return null;
+    }
+
+    @Override
+    public Set<Long> loadAllKeys() {
+        System.out.println("loadAllKeys");
+        return null;
+    }
+}
+//end::qs[]
