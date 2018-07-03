bef76fe4e9cc1e5504e3c842ccc11817ee6e41a8
Serdar Ozmen <serdar@hazelcast.com>
2018-07-03 11:52:25 +0300

    Fixing include directive

--- src/main/java/dds/reliabletopic/PublisherMember.java
+++ src/main/java/dds/reliabletopic/PublisherMember.java
@@ -5,29 +5,21 @@ import com.hazelcast.core.ITopic;
 import java.util.Random;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
-
+//tag::publisher[]
 public class PublisherMember {
-
     public static void main(String[] args) {
-//tag::pm[]
         HazelcastInstance hz = Hazelcast.newHazelcastInstance();
         Random random = new Random();
-
         ITopic<Long> topic = hz.getReliableTopic("sometopic");
         long messageId = 0;
 
         while (true) {
             topic.publish(messageId);
             messageId++;
-
             System.out.println("Written: " + messageId);
-
-            // add a bit of randomization
             sleepMillis(random.nextInt(100));
-//end::pm[]            
         }
     }
-
     public static boolean sleepMillis(int millis) {
         try {
             MILLISECONDS.sleep(millis);
@@ -38,3 +30,4 @@ public class PublisherMember {
         return true;
     }
 }
+//end::publisher[]
