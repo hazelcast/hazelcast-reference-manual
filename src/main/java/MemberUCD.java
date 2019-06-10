6385a730aa555710e1875025ae9560931b7de8be
Viliam Durina <viliam-durina@users.noreply.github.com>
2019-06-10 16:50:44 +0200

    Improve User Code Deployment part (#691)

--- src/main/java/MemberUCD.java
+++ src/main/java/MemberUCD.java
@@ -1,17 +1,40 @@
 import com.hazelcast.config.Config;
+import com.hazelcast.config.MemberAttributeConfig;
 import com.hazelcast.config.UserCodeDeploymentConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
 
 public class MemberUCD {
-    public static void main(String[] args) {
+    void ucd() {
         //tag::ucd[]
         Config config = new Config();
         UserCodeDeploymentConfig distCLConfig = config.getUserCodeDeploymentConfig();
         distCLConfig.setEnabled( true )
                 .setClassCacheMode( UserCodeDeploymentConfig.ClassCacheMode.ETERNAL )
-                .setProviderMode( UserCodeDeploymentConfig.ProviderMode.LOCAL_CLASSES_ONLY )
-                .setBlacklistedPrefixes( "com.foo" )
+                .setProviderMode( UserCodeDeploymentConfig.ProviderMode.LOCAL_AND_CACHED_CLASSES )
+                .setBlacklistedPrefixes( "com.foo,com.bar" )
                 .setWhitelistedPrefixes( "com.bar.MyClass" )
                 .setProviderFilter( "HAS_ATTRIBUTE:lite" );
         //end::ucd[]
+    }
+
+    void hasAttributeConfig() {
+        //tag::hasAttributeConfig[]
+        Config hazelcastConfig = new Config();
+        UserCodeDeploymentConfig ucdConfig = hazelcastConfig.getUserCodeDeploymentConfig();
+        ucdConfig.setProviderFilter("HAS_ATTRIBUTE:class-provider");
+
+        HazelcastInstance instance = Hazelcast.newHazelcastInstance(hazelcastConfig);
+        //end::hasAttributeConfig[]
+    }
+
+    void memberAttributeConfig() {
+        //tag::memberAttributeConfig[]
+        Config hazelcastConfig = new Config();
+        MemberAttributeConfig memberAttributes = hazelcastConfig.getMemberAttributeConfig();
+        memberAttributes.setAttribute("class-provider", "true");
+
+        HazelcastInstance instance = Hazelcast.newHazelcastInstance(hazelcastConfig);
+        //end::memberAttributeConfig[]
     }
 }
