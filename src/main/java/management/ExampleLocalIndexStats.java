b588447aa8b73896ba495001b4ea9caacb044cb8
serdaro <serdar@hazelcast.com>
2019-10-05 16:48:12 +0300

    Completing documentation build errors.

--- src/main/java/management/ExampleLocalIndexStats.java
+++ src/main/java/management/ExampleLocalIndexStats.java
@@ -1,6 +1,8 @@
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import com.hazelcast.map.IMap;
+import com.hazelcast.config.IndexConfig;
+import com.hazelcast.config.IndexType;
 import com.hazelcast.monitor.LocalIndexStats;
 import com.hazelcast.monitor.LocalMapStats;
 
@@ -12,12 +14,19 @@ public class ExampleLocalIndexStats {
         //tag::lis[]
         HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
         IMap<String, String> customers = hazelcastInstance.getMap("customers");        
-        customers.addIndex("name", true); // or add the index using the map config
+        addIndex(customers, "name", true); // or add the index using the map config
         LocalMapStats mapStatistics = customers.getLocalMapStats();
         Map<String, LocalIndexStats> indexStats = mapStatistics.getIndexStats();
         LocalIndexStats nameIndexStats = indexStats.get("name");
         System.out.println("average name index hit selectivity on this member = "
                 + nameIndexStats.getAverageHitSelectivity());
         //end::lis[]
+       
+    }
+    
+        protected static void addIndex(IMap map, String attribute, boolean ordered) {
+        IndexConfig config = new IndexConfig(ordered ? IndexType.SORTED : IndexType.HASH, attribute).setName(attribute);
+
+        map.addIndex(config);
     }
 }
