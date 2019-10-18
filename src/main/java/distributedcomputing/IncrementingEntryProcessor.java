3da11cbdf74e9ebd26113b3ce27ca9134a542372
Matko Medenjak <matko@hazelcast.com>
2019-10-18 13:57:22 +0200

    Updated entry processor sections for 4.0

--- src/main/java/distributedcomputing/IncrementingEntryProcessor.java
+++ src/main/java/distributedcomputing/IncrementingEntryProcessor.java
@@ -4,19 +4,15 @@ import java.util.Map;
 
 //tag::iep[]
 public class IncrementingEntryProcessor implements EntryProcessor<Integer, Integer, Integer> {
-
     public Integer process( Map.Entry<Integer, Integer> entry ) {
         Integer value = entry.getValue();
         entry.setValue( value + 1 );
         return value + 1;
     }
 
+    @Override
     public EntryProcessor<Integer, Integer, Integer> getBackupProcessor() {
         return IncrementingEntryProcessor.this;
-    }
-
-    public void processBackup( Map.Entry<Integer, Integer> entry ) {
-        entry.setValue( entry.getValue() + 1 );
     }
 }
 //end::iep[]
